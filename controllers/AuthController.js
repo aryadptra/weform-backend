79f0924cfe916680b8ebfb8e46e52917383c7554
Arya Dwi Putra <aryaadwptr@gmail.com>
2023-10-19 18:28:47 +0700

    request body vaidation

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -3,6 +3,18 @@ import User from "../models/User.js";
 class AuthController {
   async register(req, res) {
     try {
+      if (!req.body.fullname) {
+        throw { code: 428, message: "FULLNAME_IS_REQUIRED" };
+      }
+
+      if (!req.body.email) {
+        throw { code: 428, message: "EMAIL_IS_REQUIRED" };
+      }
+
+      if (!req.body.password) {
+        throw { code: 428, message: "PASSWORD_IS_REQUIRED" };
+      }
+
       const user = await User.create(req.body);
       if (!user) {
         throw {
