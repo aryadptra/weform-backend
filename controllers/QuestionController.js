60ed124bf860fd2b461f04b811c9f64567e7f2b6
Arya Dwi Putra <aryaadwptr@gmail.com>
2023-11-02 15:13:16 +0700

    add question update and type checking on question form

--- controllers/QuestionController.js
+++ controllers/QuestionController.js
@@ -1,6 +1,8 @@
 import mongoose from "mongoose";
 import Form from "../models/Form.js";
 
+const allowedTypes = ["Text", "Email", "Radio", "Checkbox", "Dropdown"];
+
 class QuestionController {
   async store(req, res) {
     try {
@@ -47,6 +49,76 @@ class QuestionController {
         status: true,
         message: "SUCCESS_ADD_QUESTION",
         question: newQuestion,
+      });
+    } catch (err) {
+      return res.status(err.code || 500).json({
+        status: false,
+        message: err.message,
+      });
+    }
+  }
+
+  async update(req, res) {
+    try {
+      if (!req.params.id) {
+        throw { code: 400, message: "FORM_ID_REQUIRED" };
+      }
+
+      if (!req.params.questionId) {
+        throw { code: 400, message: "QUESTION_ID_REQUIRED" };
+      }
+
+      if (!mongoose.Types.ObjectId.isValid(req.params.id)) {
+        throw { code: 400, message: "INVAlID_FORM_id" };
+      }
+
+      if (!mongoose.Types.ObjectId.isValid(req.params.questionId)) {
+        throw { code: 400, message: "INVALID_QUESTION_ID" };
+      }
+
+      let field = {};
+      if (req.body.hasOwnProperty("question")) {
+        field["questions.$[indexQuestion].question"] = req.body.question;
+      } else if (req.body.hasOwnProperty("required")) {
+        field["questions.$[indexQuestion].required"] = req.body.required;
+      } else if (req.body.hasOwnProperty("type")) {
+        if (!allowedTypes.includes(req.body.type)) {
+          throw { code: 400, message: "INVALID_TYPE" };
+        }
+        field["questions.$[indexQuestion].type"] = req.body.type;
+      }
+
+      const question = await Form.findOneAndUpdate(
+        {
+          _id: req.params.id,
+          userId: req.jwt.payload.id,
+        },
+        {
+          $set: field,
+        },
+        {
+          arrayFilters: [
+            {
+              "indexQuestion.id": new mongoose.Types.ObjectId(
+                req.params.questionId
+              ),
+            },
+          ],
+          new: true,
+        }
+      );
+
+      if (!question) {
+        throw {
+          code: 400,
+          message: "QUESTION_UPDATE_FAILED",
+        };
+      }
+
+      return res.status(200).json({
+        status: true,
+        message: "QUESTION_UPDATE_SUCCESS",
+        question,
       });
     } catch (err) {
       return res.status(err.code || 500).json({
